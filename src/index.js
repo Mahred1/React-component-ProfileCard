7b1af5fb0dc22387f1f4230bff95351982e65c77
mahred1 <mredwan.kemal@gmail.com>
2025-02-05 09:00:39 +0000

    converted the skill to a child component of skills component and added only skills component to the app component

--- src/index.js
+++ src/index.js
@@ -1,46 +1,56 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import "./styles.css"
+import "./styles.css";
 function App() {
-  return <div>
-   <Profile />
-  </div>
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
 }
 
-function Profile(){
- return <div className="profile">
-  <Avatar imgName="./gig.jpg" />
-  <Info />
+function Profile() {
+  return (
+    <div className="profile">
+      <Avatar imgName="./gig.jpg" />
+      <Info />
 
-  <div className="skills">
-  <Skill background="blue" language="HTML" emoji="👍"/>
-  <Skill background="#BF0603" language="Java Script" emoji="🦾"/>
-  <Skill background="#001427" language="React" emoji="😎"/>
-  </div>
- 
- </div>
+      <Skills />
+    </div>
+  );
 }
-function Avatar(props){
-  return <img src={props.imgName} className="avatar"></img>
+function Avatar(props) {
+  return <img src={props.imgName} className="avatar"></img>;
 }
-function Info(){
-  return <div>
-   <h1>Mahfuz Redewan</h1>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, dicta sed quisquam provident at commodi omnis quia fugit, mollitia veritatis ipsa, quo aliquid facere dignissimos?</p>
-  </div>
+function Info() {
+  return (
+    <div>
+      <h1>Mahfuz Redewan</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, dicta
+        sed quisquam provident at commodi omnis quia fugit, mollitia veritatis
+        ipsa, quo aliquid facere dignissimos?
+      </p>
+    </div>
+  );
 }
-function skills(){
-  return <div>
-  </div>
+function Skills() {
+  return (
+    <div className="skills">
+      <Skill background="blue" language="HTML" emoji="👍" />
+      <Skill background="#BF0603" language="Java Script" emoji="🦾" />
+      <Skill background="#001427" language="React" emoji="😎" />
+    </div>
+  );
 }
 
-function Skill(props){
-  return <div style={{backgroundColor:`${props.background}`}}>
-    <span>{props.language}</span>
-    <icon>{props.emoji}</icon>
-  </div>
+function Skill(props) {
+  return (
+    <div style={{ backgroundColor: `${props.background}` }}>
+      <span>{props.language}</span>
+      <icon>{props.emoji}</icon>
+    </div>
+  );
 }
-let root= ReactDom.createRoot(document.getElementById("root"));
-root.render(
-  <App />
-)
+let root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
