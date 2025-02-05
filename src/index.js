9745cdc71427257a5ea74a865face4b95a327acd
Mahred1 <mredwan.kemal@gmail.com>
2025-02-04 22:43:48 -1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./styles.css"
+function App() {
+  return <div>
+   <Profile />
+  </div>
+}
+
+function Profile(){
+ return <div className="profile">
+  <Avatar imgName="./gig.jpg" />
+  <Info />
+
+  <div className="skills">
+  <Skill background="blue" language="HTML" emoji="👍"/>
+  <Skill background="#BF0603" language="Java Script" emoji="🦾"/>
+  <Skill background="#001427" language="React" emoji="😎"/>
+  </div>
+ 
+ </div>
+}
+function Avatar(props){
+  return <img src={props.imgName} className="avatar"></img>
+}
+function Info(){
+  return <div>
+   <h1>Mahfuz Redewan</h1>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, dicta sed quisquam provident at commodi omnis quia fugit, mollitia veritatis ipsa, quo aliquid facere dignissimos?</p>
+  </div>
+}
+function skills(){
+  return <div>
+  </div>
+}
+
+function Skill(props){
+  return <div style={{backgroundColor:`${props.background}`}}>
+    <span>{props.language}</span>
+    <icon>{props.emoji}</icon>
+  </div>
+}
+let root= ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <App />
+)
